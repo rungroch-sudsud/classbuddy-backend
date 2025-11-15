5c5e4203f2892a1e4fefc97ad0cdcb5d790447df
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2025-11-15 15:20:39 +0700

    fix(user) : get user points from available balance from wallet

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,13 +5,14 @@ import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';import { S3Module } from 'src/infra/s3/s3.module';
 import { Teacher, TeacherSchema } from '../teachers/schemas/teacher.schema';
 import { ChatModule } from '../chat/chat.module';
-;
+import { Wallet, WalletSchema } from '../payments/schemas/wallet.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
-      { name: Teacher.name, schema: TeacherSchema }
+      { name: Teacher.name, schema: TeacherSchema },
+      { name: Wallet.name, schema: WalletSchema }
     ]),
     S3Module,
     ChatModule
