29c626fcabe85bfb92b18f7e371c1dc6c3da7e5c
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2025-11-30 22:52:11 +0700

    chore(post) : adjust from 200 to 2000 for post detail

--- src/modules/posts/dto/post.dto.ts
+++ src/modules/posts/dto/post.dto.ts
@@ -6,29 +6,27 @@ export const CreatePostSchema = z.object({
         .string()
         .min(1, 'กรุณากรอกรายละเอียดของโพส')
         .min(30, 'ข้อความของโพสต้องมีอย่างน้อย 30 ตัวอักษร')
-        .max(200, 'ข้อความของโพสต้องมีอย่างน้อย 30 ตัวอักษร')
+        .max(2000, 'ข้อความของโพสต้องมีอย่างน้อย 2000 ตัวอักษร'),
 });
 
 export class CreatePostDto extends createZodDto(CreatePostSchema) {}
-
 
 export const UpdatePostSchema = z.object({
     detail: z
         .string()
         .min(30, 'ข้อความของโพสต้องมีอย่างน้อย 30 ตัวอักษร')
-        .max(200, 'ข้อความของโพสต้องมีอย่างน้อย 30 ตัวอักษร')
-        .optional()
+        .max(2000, 'ข้อความของโพสต้องมีอย่างน้อย 2000 ตัวอักษร')
+        .optional(),
 });
 
 export class UpdatePostDto extends createZodDto(UpdatePostSchema) {}
-
 
 export const CreateProposalSchema = z.object({
     detail: z
         .string()
         .min(1, 'กรุณากรอกรายละเอียดของโพส')
         .min(30, 'ข้อความของเสนอต้องมีอย่างน้อย 30 ตัวอักษร')
-        .max(200, 'ข้อความของเสนอต้องมีอย่างน้อย 30 ตัวอักษร')
+        .max(2000, 'ข้อความของเสนอต้องมีอย่างน้อย 2000 ตัวอักษร'),
 });
 
 export class CreateProposalDto extends createZodDto(CreateProposalSchema) {}
