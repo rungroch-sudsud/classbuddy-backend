fda7e3f2dc10910c69c0d08998010409a9ddb6ed
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2025-12-08 20:29:54 +0700

    chore : remove unused codes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,96 +1,84 @@
 import {
-  Body,
-  Param,
-  Controller,
-  Get,
-  Post,
-  Patch,
-  UploadedFile,
-  UseGuards,
+    Body,
+    Controller,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UploadedFile,
+    UseGuards,
 } from '@nestjs/common';
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiConsumes,
-  ApiTags,
-} from '@nestjs/swagger';
-import { UsersService } from './users.service';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { UploadFileDto } from 'src/shared/docs/upload.file.docs';
+import { UploadInterceptor } from 'src/shared/interceptors/upload.interceptor';
 import { CurrentUser } from 'src/shared/utils/currentUser';
 import { JwtGuard } from '../auth/guard/auth.guard';
 import { UpdateProfileDto } from './schemas/user.zod.schema';
-import { ZodFilePipe } from 'src/shared/validators/zod.validation.pipe';
-import { UploadInterceptor } from 'src/shared/interceptors/upload.interceptor';
-import { ImageFileSchema } from 'src/shared/validators/zod.schema';
-import { UploadFileDto } from 'src/shared/docs/upload.file.docs';
-
+import { UsersService } from './users.service';
 
 @ApiTags('Users')
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService
-  ) { }
+    constructor(private readonly usersService: UsersService) {}
 
+    @Patch('profile')
+    @UseGuards(JwtGuard)
+    @ApiBody({ type: UpdateProfileDto })
+    async updateProfile(
+        @CurrentUser() userId: string,
+        @Body() body: UpdateProfileDto,
+    ) {
+        const user = await this.usersService.updateProfile(userId, body);
 
-  @Patch('profile')
-  @UseGuards(JwtGuard)
-  @ApiBody({ type: UpdateProfileDto })
-  async updateProfile(
-    @CurrentUser() userId: string,
-    @Body() body: UpdateProfileDto
-  ) {
-    const user = await this.usersService.updateProfile(userId, body);
+        return {
+            message: 'อัพเดทข้อมูลของฉันสำเร็จ',
+            data: user,
+        };
+    }
 
-    return {
-      message: 'อัพเดทข้อมูลของฉันสำเร็จ',
-      data: user,
-    };
-  }
+    @Post('profile/image')
+    @ApiBody({ type: UploadFileDto })
+    @ApiConsumes('multipart/form-data')
+    @UseGuards(JwtGuard)
+    @UploadInterceptor('file', 1, 5)
+    async uploadProfileImage(
+        @CurrentUser() userId: string,
+        @UploadedFile() file: Express.Multer.File,
+    ) {
+        const update = await this.usersService.updateProfileImage(userId, file);
 
+        return {
+            message: 'อัพเดทรูปโปรไฟล์ของฉันสำเร็จ',
+            data: update,
+        };
+    }
 
-  @Post('profile/image')
-  @ApiBody({ type: UploadFileDto })
-  @ApiConsumes('multipart/form-data')
-  @UseGuards(JwtGuard)
-  @UploadInterceptor('file', 1, 5)
-  async uploadProfileImage(
-    @CurrentUser() userId: string,
-    @UploadedFile() file: Express.Multer.File,
-  ) {
-    const update = await this.usersService.updateProfileImage(userId, file);
+    @Get('mine')
+    @UseGuards(JwtGuard)
+    async getMe(@CurrentUser() userId: string) {
+        const user = await this.usersService.getUserProfileMine(userId);
 
-    return {
-      message: 'อัพเดทรูปโปรไฟล์ของฉันสำเร็จ',
-      data: update,
-    };
-  }
+        return {
+            message: 'ดึงโปรไฟล์ของฉันสำเร็จ',
+            data: user,
+        };
+    }
 
+    @Patch('bookmarks/:teacherId')
+    @UseGuards(JwtGuard)
+    async toggleBookmark(
+        @CurrentUser() userId: string,
+        @Param('teacherId') teacherId: string,
+    ) {
+        const booked = await this.usersService.toggleBookmark(
+            userId,
+            teacherId,
+        );
 
-  @Get('mine')
-  @UseGuards(JwtGuard)
-  async getMe(@CurrentUser() userId: string) {
-    const user = await this.usersService.getUserProfileMine(userId);
-
-    return {
-      message: 'ดึงโปรไฟล์ของฉันสำเร็จ',
-      data: user,
-    };
-  }
-
-
-  @Patch('bookmarks/:teacherId')
-  @UseGuards(JwtGuard)
-  async toggleBookmark(
-    @CurrentUser() userId: string,
-    @Param('teacherId') teacherId: string,
-  ) {
-    const booked = await this.usersService.toggleBookmark(userId, teacherId);
-
-    return {
-      message: 'บันทึกข้อมูลเรียบร้อย',
-      data: booked,
-    };
-  }
-
+        return {
+            message: 'บันทึกข้อมูลเรียบร้อย',
+            data: booked,
+        };
+    }
 }
