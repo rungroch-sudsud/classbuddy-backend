af92896fa26f8037aa112e8877f879f915ea1f6e
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2025-12-12 14:39:18 +0700

    chore : log teacher

--- src/modules/slots/slots.service.ts
+++ src/modules/slots/slots.service.ts
@@ -343,7 +343,8 @@ export class SlotsService {
         });
 
         return sorted.map(({ startTime, endTime, date, booking, ...rest }) => {
-            const teacher: any = booking.teacher;
+            const teacher: any = booking.teacherId;
+            console.log('teacher', teacher);
             const startLocal = dayjs.utc(startTime).tz('Asia/Bangkok');
             const endLocal = dayjs.utc(endTime).tz('Asia/Bangkok');
 
